5ef48e63ba821a876902601228096611cc2344fd
Harsh Shah <shahharsh.210995@gmail.com>
2025-03-31 22:48:40 +0530

    API method

--- main.tf
+++ main.tf
@@ -12,3 +12,20 @@ resource "aws_api_gateway_rest_api" "sns_api" {
   name        = var.sns_rest_api_name
   description = "API Gateway to trigger SNS Email Notification"
 }
+
+resource "aws_api_gateway_resource" "sns_resource" {
+  rest_api_id = aws_api_gateway_rest_api.sns_api.id
+  parent_id   = aws_api_gateway_rest_api.sns_api.root_resource_id
+  path_part   = "sns"
+}
+
+resource "aws_api_gateway_method" "sns_post" {
+  rest_api_id   = aws_api_gateway_rest_api.sns_api.id
+  resource_id   = aws_api_gateway_resource.sns_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+  request_parameters = {
+    "method.request.querystring.Message"  = true,
+    "method.request.querystring.TopicArn" = true,
+  }
+}
